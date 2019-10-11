9221a19b794d62aeb1290de844b295e94e7a18b8
pc casa <sfdelgadop@unal.edu.co>
2019-10-11 02:31:50 -0500

    organizando toda la aplicación y añadiendo la visualización de las recetas

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent'
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
-  }
+
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Master Chief</NavbarBrand>
-          </div>
-        </Navbar>
-
-        <div className = "row">
-          <div  className="col-9">
-            <Menu dishes={this.state.dishes} />
-          </div>
-          <div  className="col-3">
-            <p>Perfil</p>
-            <p>Filtros</p>
-            <p>Cuadros bonitos</p>
-          </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
         </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
