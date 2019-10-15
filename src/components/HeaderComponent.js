7ce10248810c8305e576eaa20ebd0cad5050afac
unknown <nfgavilans@unal.edu.co>
2019-10-14 19:03:59 -0500

    Version 1.0.1

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import { MDBInput, MDBCol,MDBFormInline,MDBIcon,MDBBtn } from "mdbreact";
 
 class Header extends Component {
     constructor(props) {
@@ -21,26 +22,33 @@ class Header extends Component {
     render() {
         return(
             <div>
-                <Navbar dark expand="md">
+                <Navbar dark expand="md" style={{backgroundColor: '#5bc0de'}}>
                     <div className="container">
-                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarToggler onClick={this.toggleNav}  />
                         <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
+                        
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                             <NavItem>
-                                <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
+                                <NavLink /*style={{color: 'white'}}*/ className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> Acerca de nosotros</NavLink>
+                                <NavLink /*style={{color: 'white'}}*/className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> Acerca de nosotros</NavLink>
+                            </NavItem>  
+                            <NavItem>
+                                <NavLink /*style={{color: 'white'}}*/ className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span> Recetas</NavLink>
                             </NavItem>
                             <NavItem>
-                                <NavLink className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span> Recetas</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contáctanos</NavLink>
+                                <NavLink /*style={{color: 'white'}}*/ className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contáctanos</NavLink>
                             </NavItem>
                             </Nav>
                         </Collapse>
+                        <MDBCol md="15">
+                            <MDBFormInline className="md-form">
+                                <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+                            </MDBFormInline>
+                        </MDBCol>                        
+                        <a  href="Al componente login supongo :V"><img src='assets/images/Login.png' height="50" width="70" alt='Ristorante Con Fusion' /> </a>
                     </div>
                 </Navbar>
             </div>
