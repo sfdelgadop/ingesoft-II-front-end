60a88044c82aee38485ee44c835d745a565d8087
pc casa <sfdelgadop@unal.edu.co>
2019-10-14 22:38:47 -0500

    fixing errors

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -6,8 +6,6 @@ import { MDBInput, MDBCol,MDBFormInline,MDBIcon,MDBBtn } from "mdbreact";
 class Header extends Component {
 	constructor(props) {
 		super(props);
-
-		this.toggleNav = this.toggleNav.bind(this);
 		this.state = {
 			isNavOpen: false
 		};
@@ -16,24 +14,14 @@ class Header extends Component {
     render() {
         return(
             <div>
-                <Navbar dark expand="md" style={{backgroundColor: '#5bc0de'}}>
+                <Navbar dark expand="md">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav}  />
-                        <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
                         
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                             <NavItem>
-                                <NavLink /*style={{color: 'white'}}*/ className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink /*style={{color: 'white'}}*/className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> Acerca de nosotros</NavLink>
-                            </NavItem>  
-                            <NavItem>
                                 <NavLink /*style={{color: 'white'}}*/ className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span> Recetas</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink /*style={{color: 'white'}}*/ className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contáctanos</NavLink>
                             </NavItem>
                             </Nav>  
                         </Collapse>
